5cfcc497e0450919af5c8dc840702cbf757e743f
mike-k-git <mikehsm@outlook.com>
2022-04-10 13:10:26 +0300

    uibuld

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
